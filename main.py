557c7f6125c4d54ad609591487c438c940e15a2e
omar123-jav <omarosama818@gmail.com>
2023-02-28 14:57:16 +0200

    state  vallue done

--- main.py
+++ main.py
@@ -13,9 +13,10 @@ def generate_grid():
    gridreward[0][3]=1
    gridreward[1][3]=-1
    return grid,gridreward
-def state_value(grid):
- equations=np.zeros(shape=(9,9),dtype=int)
- constants=np.zeros(shape=(1,9),dtype=int)
+def state_value(grid,d):
+ equations=np.zeros(shape=(9,9))
+ constants=np.array([0,0,0,0,0,0,0,0,0],dtype=float)
+ #print(constants)
  for i in range(0,3):
     for j in range(0,4):
        if( (i==0 and j==3) or (i==1 and j==3) or(i==1 and j==1)):
@@ -24,54 +25,123 @@ def state_value(grid):
        i2=i+1
        j1=j-1
        j2=j+1
-       if i1<0:       #up movement
-          equations[j][j]=0.9
+       if i1<0:
+
+          equations[j][j]+=0.25*d
+          #print(equations[j][j])
        else:
           if  not (i1==1 and j==1):
             if i==1:
                if j==0:
-                equations[3][0]+=0.9
-                constants[3]+=grid[i1][j]
+                equations[3][0]+=0.25*d
+                #constants[3]+=grid[i1][j]
 
                elif j==2:
-                  equations[4][2]+=0.9
-                  constants[4]+=grid[i1][j]
+                  equations[4][2]+=0.25*d
+                  #constants[4]+=grid[i1][j]
 
 
 
             else:
              if j==0:
-               equations[5 + j][3+j] += 0.9
-               constants[5 + j] += grid[i1][j]
+               equations[5 ][3] += 0.25*d
+               #constants[5] += -grid[i1][j]
 
-       if i2>2:
-          equations[5+j][5+j]=0.9
-       else:
-          if not (i1 == 1 and j == 1):
-             if i == 1:
-
-                equations[3 + j][j] += 0.9
-                constants[3 + j] += grid[i2][j]
+             elif j==2:
+                equations[7][4]+=0.25*d
+                #constants[7] += -grid[i1][j]
              else:
-                equations[ j][j] += 0.9
-                constants[j] += grid[i2][j]
+                 constants[8]+=0.25
+          else:
 
+             equations[6][6]+=0.25*d
+       #print("1",equations[5][5])
+       if i2>2:
+
+          equations[5+j][5+j]+=0.25*d
+          #print("2",equations[5][5])
+       else:
+          if not (i2 == 1 and j == 1):
+             if i == 1:
+                if j==0:
+                 equations[3][5] += 0.25*d
+                 #constants[3] += -grid[i2][j]
+                else:
+                   equations[4][7]+=0.25*d
+             else:
+                if j==0:
+                 equations[0][3] += 0.25*d
+                 #constants[0] += -grid[i2][j]
+                else:
+                   equations[2][4]+=0.25*d
+                   #constants[2]+=-grid[i2][j]
+          else:
+             equations[1][1]+=0.25*d
+          #print(3,equations[5][5])
 
        if j2>3:
-          equations[8][8]+=0.9
+          equations[8][8]+=0.25*d
+
+       else:
+         if not(i==1 and j2==1):
+             if i==0:
+                 if j2==3:
+                  constants[2]+=-0.25
+                 else:
+                     equations[j][j2]+=0.25*d
+                     #print(j,j2)
+             elif i==1:
+               constants[4]+=0.25
+             else:
+              equations[5+j][5+j2]+=0.25*d
+              #constants[5+j]+=grid[i][j2]
+         else:
+             equations[3][3]+=0.25*d
+         #print(4,equations[5][5])
+
        if j1<0:
           if i==0:
-             equations[0][0]+=0.9
-          if i==1:
-             equations[3][3]+=0.9
+             equations[0][0]+=0.25*d
+          elif i==1:
+             equations[3][3]+=0.25*d
           else:
-             equations[5][5]+=0.9
+             equations[5][5]+=0.25*d
+          #print(5,equations[5][5])
+       else:
+           if not (i == 1 and j1 == 1):
+               if i==0:
+                   equations[j][j1]+=0.25*d
+               else:
+                   equations[5+j][5+j1]+=0.25*d
+           else:
+               equations[4][4]+=0.25*d
+
+# print(equations)
+ for i in range(0,9):
+     equations[i][i]+=-1
+ #print(equations)
+ #print(constants)
+ solutions=np.linalg.solve(equations,constants)
+ statevalues=np.zeros(shape=(3,4))
+ #print(solutions)
+ statevalues[0][0]=solutions[0]
+ statevalues[0][1] = solutions[1]
+ statevalues[0][2] = solutions[2]
+ statevalues[0][3]=0
+ statevalues[1][0]=solutions[3]
+ statevalues[1][1]=0
+ statevalues[1][2]=solutions[4]
+ statevalues[1][3] = 0
+ statevalues[2][0]=solutions[5]
+ statevalues[2][1]=solutions[6]
+ statevalues[2][2]=solutions[7]
+ statevalues[2][3]=solutions[8]
+ return statevalues
 
 
+if __name__ == '__main__':
 
-
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-grid,gridreward=generate_grid()
-print(gridreward)
+ # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+ grid,gridreward=generate_grid()
+ state_values=state_value(gridreward,0.9)
+ #print(state_values)
