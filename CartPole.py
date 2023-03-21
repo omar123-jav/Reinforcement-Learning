389e5953c8e376e21d4ce507450cd0abf0789feb
omar123-jav <omarosama818@gmail.com>
2023-03-21 21:37:22 +0200

    state_idx+randomarray

--- CartPole.py
+++ CartPole.py
@@ -3,50 +3,89 @@ import random
 import gym
 import numpy as np
 def state_idx(position,angle):
-     if position>=-4.8 and position<-2.4:
-         if angle>=-4.2 and  angle<-2.115:
-             return 0
-         elif angle>=-2.115 and angle<-0.01:
+     # if position>=-4.8 and position<-2.4:
+     #     if angle>=-4.2 and  angle<-2.115:
+     #         return 0
+     #     elif angle>=-2.115 and angle<-0.01:
+     #         return 1
+     #     elif angle>=-0.01 and angle< 0.2095:
+     #         return 2
+     #     else:
+     #         return 3
+     # elif position>=-2.4 and position<0:
+     #     if angle>=-4.2 and  angle<-2.115:
+     #         return 4
+     #     elif angle>=-2.115 and angle<-0.01:
+     #         return 5
+     #     elif angle>=-0.01 and angle< 0.2095:
+     #         return 6
+     #     else:
+     #         return 7
+     # elif position>=0 and position<2.4:
+     #     if angle>=-4.2 and  angle<-2.115:
+     #         return 8
+     #     elif angle>=-2.115 and angle<-0.01:
+     #         return 9
+     #     elif angle>=-0.01 and angle< 0.2095:
+     #         return 10
+     #     else:
+     #         return 11
+     # else:
+     #     if angle >= -4.2 and angle < -2.115:
+     #         return 12
+     #     elif angle >= -2.115 and angle < -0.01:
+     #         return 13
+     #     elif angle >= -0.01 and angle < 0.2095:
+     #         return 14
+     #     else:
+     #         return 15
+     if position>2.4 or position<-2.4 or angle>0.2095 or angle<-0.2095:
+         return 0
+     elif position<=2.4 and position>1.2:
+         if angle<=0.2095 and angle>0.10475:
              return 1
-         elif angle>=-0.01 and angle< 0.2095:
+         elif angle <=0.10475 and angle>0:
              return 2
-         else:
+         elif angle<=0 and  angle>-0.10475:
              return 3
-     elif position>=-2.4 and position<0:
-         if angle>=-4.2 and  angle<-2.115:
+         else:
              return 4
-         elif angle>=-2.115 and angle<-0.01:
+     elif position <= 1.2 and position > 0:
+         if angle <= 0.2095 and angle > 0.10475:
              return 5
-         elif angle>=-0.01 and angle< 0.2095:
+         elif angle <= 0.10475 and angle > 0:
              return 6
-         else:
+         elif angle <= 0 and angle > -0.10475:
              return 7
-     elif position>=0 and position<2.4:
-         if angle>=-4.2 and  angle<-2.115:
+         else:
              return 8
-         elif angle>=-2.115 and angle<-0.01:
+     elif position <= 0 and position > -1.2:
+         if angle <= 0.2095 and angle > 0.10475:
              return 9
-         elif angle>=-0.01 and angle< 0.2095:
+         elif angle <= 0.10475 and angle > 0:
              return 10
-         else:
+         elif angle <= 0 and angle > -0.10475:
              return 11
-     else:
-         if angle >= -4.2 and angle < -2.115:
-             return 12
-         elif angle >= -2.115 and angle < -0.01:
-             return 13
-         elif angle >= -0.01 and angle < 0.2095:
-             return 14
          else:
+             return 12
+     else:
+         if angle <= 0.2095 and angle > 0.10475:
+             return 13
+         elif angle <= 0.10475 and angle > 0:
+             return 14
+         elif angle <= 0 and angle > -0.10475:
              return 15
+         else:
+             return 16
 
 
 
 
 env=gym.make('CartPole-v1')
-qtable=np.zeros(shape=(16,2))
+qtable=np.random.rand(17,2)
 episode=1
 epsilone=0.05
+rewards=[]
 #print(qtable)
 for _ in range(500):
     state,info = env.reset()
@@ -86,6 +125,7 @@ for _ in range(500):
 
       qtable[st_idx][action]=qtable[st_idx][action]+0.1*(reward+np.max(qtable[state_next_idx])-qtable[st_idx][action])
       state=state_next
+
       #print(state_next,reward)
 
     episode+=1
